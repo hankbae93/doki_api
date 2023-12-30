e213dbbd7651da8abe1de0f18385b4c9c5e1548b
Hank <seolin9340@gmail.com>
2023-12-30 19:09:06 +0900

    remove: Agenda Modules

--- src/agenda/dto/update-agenda.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAgendaDto } from './create-agenda.dto';
-
-export class UpdateAgendaDto extends PartialType(CreateAgendaDto) {}
--- src/agenda/dto/vote-agenda.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
-
-export class VoteAgendaDto {
-  @IsNumber()
-  @IsNotEmpty()
-  optionId: number;
-}
